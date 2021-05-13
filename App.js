b8210a519cbe8b15ddf9c78eddb708e20b71c47c
Emad <emad998@yahoo.com>
2021-05-13 09:34:18 -0700

    finished functional component chapter

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Hello from './components/Hello';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Hello />
+      
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function Hello() {
+    return (
+        <View>
+            <Text>Hello from functional component</Text>
+            <Text>This is the second text</Text>
+        </View>
+    )
+}
+
+export default Hello
